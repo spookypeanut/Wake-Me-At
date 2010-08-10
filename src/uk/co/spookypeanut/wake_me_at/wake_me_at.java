5ce74d8e6b09c8362bba019bdb04aebcf3ef4217
spookypeanut <github@spookypeanut.co.uk>
2010-08-10 22:50:42 +0100

    First commit, creates a map

---
+++ src/uk/co/spookypeanut/wake_me_at/wake_me_at.java
@@ -0,0 +1,24 @@
+package uk.co.spookypeanut.wake_me_at;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+
+public class wake_me_at extends MapActivity{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //MapView mapView = (MapView) findViewById(R.id);
+//        mapView.setBuiltInZoomControls(true);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
