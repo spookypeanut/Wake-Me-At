517e02abb0ce22e3e22d06dbd05991207de1ec2c
spookypeanut <github@spookypeanut.co.uk>
2010-08-16 23:32:11 +0100

    First version of interface, button clicks

---
+++ src/uk/co/spookypeanut/wake_me_at/get_location.java
@@ -0,0 +1,23 @@
+package uk.co.spookypeanut.wake_me_at;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class get_location extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
--- src/uk/co/spookypeanut/wake_me_at/wake_me_at.java
+++ src/uk/co/spookypeanut/wake_me_at/wake_me_at.java
@@ -2,23 +2,34 @@ package uk.co.spookypeanut.wake_me_at;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
+//Create an anonymous implementation of OnClickListener
 
-public class wake_me_at extends MapActivity{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        MapView mapView = (MapView) findViewById(R.id.mapview);
-        mapView.setBuiltInZoomControls(true);
-    }
-    @Override
-    protected boolean isRouteDisplayed() {
-        // TODO Auto-generated method stub
-        return false;
-    }
 
+public class wake_me_at extends Activity {
+	private OnClickListener mCorkyListener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	Toast toast = Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT);
+	    	toast.show();
+	    	get_location getlocation = new get_location();
+	    }
+	};
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	    // Capture our button from layout
+	    Button button = (Button)findViewById(R.id.getLocationButton);
+	    // Register the onClick listener with the implementation above
+	    button.setOnClickListener(mCorkyListener);
+
+	}
 }
