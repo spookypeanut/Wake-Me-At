0354c8b84e362c01d5baad6089d10fec8f9a0c78
spookypeanut <github@spookypeanut.co.uk>
2010-08-22 21:21:33 +0100

    Got the overlay working
    Again, straight from the tutorial.
    Need to move the "hot-point" of the overlay so it's at the centre.

--- src/uk/co/spookypeanut/wake_me_at/GetLocation.java
+++ src/uk/co/spookypeanut/wake_me_at/GetLocation.java
@@ -1,9 +1,15 @@
 package uk.co.spookypeanut.wake_me_at;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class GetLocation extends MapActivity {
     /** Called when the activity is first created. */
@@ -13,6 +19,13 @@ public class GetLocation extends MapActivity {
         setContentView(R.layout.get_location);
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.x);
+        MapOverlay itemizedoverlay = new MapOverlay(drawable);
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
     }
     @Override
     protected boolean isRouteDisplayed() {
