c5aa6bcd1a4676aaff1f6a02decc6a1ca1d7ad97
spookypeanut <github@spookypeanut.co.uk>
2010-08-22 20:53:44 +0100

    Add MapOverlay.java
    Straight from http://developer.android.com/resources/tutorials/views/hello-mapview.html
    Doesn't do anything yet, but it compiles

---
+++ src/uk/co/spookypeanut/wake_me_at/MapOverlay.java
@@ -0,0 +1,60 @@
+package uk.co.spookypeanut.wake_me_at;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * @author hbush
+ *
+ */
+public class MapOverlay extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	Context mContext;
+	
+	public MapOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+	  return mOverlays.get(i);
+	}
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.ItemizedOverlay#createItem(int)
+	 */
+
+
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.ItemizedOverlay#size()
+	 */
+	@Override
+	public int size() {
+	  return mOverlays.size();
+	}
+	
+	public MapOverlay(Drawable defaultMarker, Context context) {
+		  super(defaultMarker);
+		  mContext = context;
+		}
+	
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+}
