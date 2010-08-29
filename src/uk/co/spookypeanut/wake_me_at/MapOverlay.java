d2b7b2488090bdd79a04cdfaf02a55ed72abf416
spookypeanut <github@spookypeanut.co.uk>
2010-08-29 21:31:02 +0100

    Pop up the first line of the address when long-pressing in the map view

--- src/uk/co/spookypeanut/wake_me_at/MapOverlay.java
+++ src/uk/co/spookypeanut/wake_me_at/MapOverlay.java
@@ -18,13 +18,20 @@ package uk.co.spookypeanut.wake_me_at;
     <http://www.gnu.org/licenses/>.
 */
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.GestureDetector.OnGestureListener;
 import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
@@ -32,54 +39,97 @@ import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
-public class MapOverlay extends ItemizedOverlay {
+public class MapOverlay extends ItemizedOverlay implements OnGestureListener {
+	private GestureDetector gestureDetector;
+	private MapView mapView;
+
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
 
 	public MapOverlay(Drawable defaultMarker, Context context) {
-	  super(boundCenter(defaultMarker));
-	  mContext = context;
+		super(boundCenter(defaultMarker));
+		mContext = context;
+		gestureDetector = new GestureDetector(this);
 	}
-	
+
 	public MapOverlay(Drawable defaultMarker) {
 		super(boundCenter(defaultMarker));
 	}
 
 	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+		mOverlays.add(overlay);
+		populate();
 	}
-	
+
 	@Override
 	protected OverlayItem createItem(int i) {
-	  return mOverlays.get(i);
+		return mOverlays.get(i);
 	}
 
 	@Override
 	public int size() {
-	  return mOverlays.size();
+		return mOverlays.size();
 	}
-	
+
+	public boolean onTouchEvent(MotionEvent event, MapView mv) {
+		mapView = mv;
+		if (gestureDetector.onTouchEvent(event)) {
+			return true;
+		}
+		return false;
+	}
+
 	@Override
-	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
-	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setMessage(item.getSnippet());
-	  dialog.show();
-	  return true;
+	public void onLongPress(MotionEvent event) {
+		Geocoder geoCoder = new Geocoder(mContext, Locale.getDefault());
+		GeoPoint p = mapView.getProjection().fromPixels(
+				(int) event.getX(),
+				(int) event.getY());
+
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle("Location");
+		List<Address> addresses = null;
+		try {
+			addresses = geoCoder.getFromLocation(
+					p.getLatitudeE6()  / 1E6, 
+					p.getLongitudeE6() / 1E6, 1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String address = addresses.get(0).getAddressLine(0);
+		dialog.setMessage(address);
+		dialog.show();
 	}
-    public boolean onTouchEvent(MotionEvent event, MapView mapView) 
-    {   
-        if (event.getAction() == 1) {
-            GeoPoint p = mapView.getProjection().fromPixels(
-                (int) event.getX(),
-                (int) event.getY());
-            Toast.makeText(mContext, 
-                           p.getLatitudeE6() / 1E6 + "," + 
-                           p.getLongitudeE6() /1E6 , 
-                           Toast.LENGTH_SHORT).show();
-        }                            
-        return false;
-    } 
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	} 
 }
