01a9e24c6e5dd8ed9036eb5b1dbba300cdb630dd
spookypeanut <github@spookypeanut.co.uk>
2010-08-30 15:01:19 +0100

    Refactor: overlay in with mapactivity
    The only way to get the mapactivity to be able to return a value,
    as far as I can see, is for it to be a sub-class of the mapactivity.

--- src/uk/co/spookypeanut/wake_me_at/GetLocation.java
+++ src/uk/co/spookypeanut/wake_me_at/GetLocation.java
@@ -17,11 +17,14 @@ package uk.co.spookypeanut.wake_me_at;
     along with Wake Me At, in the file "COPYING".  If not, see 
     <http://www.gnu.org/licenses/>.
 */
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Criteria;
@@ -30,9 +33,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.GestureDetector.OnGestureListener;
 import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
@@ -177,5 +184,109 @@ public class GetLocation extends MapActivity
 		// TODO Auto-generated method stub
 		
 	}
+	public class MapOverlay extends ItemizedOverlay<OverlayItem> implements OnGestureListener {
+		private GestureDetector gestureDetector;
+		private MapView mapView;
+
+		private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+		private Context mContext;
+
+		public MapOverlay(Drawable defaultMarker, Context context) {
+			super(boundCenter(defaultMarker));
+			mContext = context;
+			gestureDetector = new GestureDetector(this);
+		}
+
+		public MapOverlay(Drawable defaultMarker) {
+			super(boundCenter(defaultMarker));
+		}
+
+		public void addOverlay(OverlayItem overlay) {
+			mOverlays.add(overlay);
+			populate();
+		}
+
+		@Override
+		protected OverlayItem createItem(int i) {
+			return mOverlays.get(i);
+		}
+
+		@Override
+		public int size() {
+			return mOverlays.size();
+		}
+
+		public boolean onTouchEvent(MotionEvent event, MapView mv) {
+			mapView = mv;
+			if (gestureDetector.onTouchEvent(event)) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent event) {
+			Geocoder geoCoder = new Geocoder(mContext, Locale.getDefault());
+			GeoPoint p = mapView.getProjection().fromPixels(
+					(int) event.getX(),
+					(int) event.getY());
+
+			AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+			dialog.setTitle("Location");
+			List<Address> addresses = null;
+			try {
+				addresses = geoCoder.getFromLocation(
+						p.getLatitudeE6()  / 1E6, 
+						p.getLongitudeE6() / 1E6, 1);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			String address = "";
+			if (addresses.size() > 0) 
+	        {
+	            for (int i=0; i<addresses.get(0).getMaxAddressLineIndex(); 
+	                 i++)
+	               address += addresses.get(0).getAddressLine(i) + "\n";
+	        }
+
+			dialog.setMessage(address);
+			dialog.show();
+	        setResult(RESULT_OK, (new Intent()).setAction("passing it"));
+	        //finish();
+		}
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+				float distanceY) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void onShowPress(MotionEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent e) {
+			// TODO Auto-generated method stub
+			return false;
+		} 
+	}
+
 
 }
