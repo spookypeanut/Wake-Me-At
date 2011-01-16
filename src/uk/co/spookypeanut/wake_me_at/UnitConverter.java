38b73da30423046a3993df0629ddfaf2f1cfe439
spookypeanut <github@spookypeanut.co.uk>
2011-01-16 20:26:30 +0000

    Implement the unit systems a different way
    (Which works)

--- src/uk/co/spookypeanut/wake_me_at/UnitConverter.java
+++ src/uk/co/spookypeanut/wake_me_at/UnitConverter.java
@@ -18,6 +18,7 @@ along with Wake Me At, in the file "COPYING".  If not, see
 <http://www.gnu.org/licenses/>.
 */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -29,31 +30,30 @@ import android.util.Log;
 public class UnitConverter
 {
     public final String LOG_NAME = WakeMeAt.LOG_NAME;
-
-    Context mContext;
-    Resources mRes;
-
-    
-    private final List <Unit> mUnitList =
-            Arrays.asList(
-                new Unit("metre", "m", 1, "km", null),
-                new Unit("foot", "ft", 0.3048, "mi", null),
-                new Unit("kilometre", "km", 1000, null, "m"),
-                new Unit("mile", "mi", 1609.344, null, "ft"));
-
-    Unit mUnit;
+    private final int SYSTEM_METRIC = 1;
+    private final int SYSTEM_IMPERIAL = 2;
     
     // Number of decimal places to round to
     static final int DP = 2;
     
-    private double[] stringArrayToDoubleArray(String[] inputArray) {
-        double[] returnArray = new double[inputArray.length];
-        int i;
-        for(i=0; i < inputArray.length; i++) {
-            returnArray[i] = Double.valueOf(inputArray[i]);
-        }
-        return returnArray;
-    }
+    // The value that determines which unit in a unit system will be used
+    private final double LARGESTNUM = 500;
+
+    Context mContext;
+    Resources mRes;
+    
+    // Metre is a special unit, as it's the one that's used internally
+    private final Unit mMetreUnit = new Unit("metre", "m", 1, SYSTEM_METRIC);
+    
+    private final List <Unit> mUnitList = 
+            Arrays.asList(
+                mMetreUnit,
+                new Unit("foot", "ft", 0.3048, SYSTEM_IMPERIAL),
+                new Unit("kilometre", "km", 1000, SYSTEM_METRIC),
+                new Unit("mile", "mi", 1609.344, SYSTEM_IMPERIAL));
+
+    Unit mUnit;
+    
     
     public UnitConverter(Context context, String unitAbbrev) {
         Unit unit = getFromAbbrev(unitAbbrev);
@@ -93,8 +93,11 @@ public class UnitConverter
     
     public String out(double value) {
         // 0 is always metres (used internally)
-        double rawValue = roundToDecimals(toUnit(value), DP);
-        return "" + roundToDecimals(toUnit(value), DP) + mUnit.getAbbrev();
+        Unit bestUnit = getBestUnit(value);
+        double outValue = convert(value, mMetreUnit, bestUnit);
+        String outAbbrev = bestUnit.getAbbrev();
+        
+        return String.format("%." + DP + "f%s", outValue, outAbbrev);
     }
     
     public void switchUnit(Unit unit) {
@@ -110,19 +113,58 @@ public class UnitConverter
         return value * sourceUnit.getValue() / destUnit.getValue();
     }
     
+    private ArrayList<Unit> unitsFromSystem(int system) {
+        ArrayList<Unit> returnList = new ArrayList<Unit>();
+        for (Iterator<Unit> i = mUnitList.iterator(); i.hasNext();) {
+            Unit currUnit = i.next();
+            int currSystem = currUnit.getSystem();
+            if (currSystem == system) {
+                returnList.add(currUnit);
+            }
+        }
+        return returnList;
+    }
+    
+    private Unit getBestUnit(double srcValue) {
+        Unit currBestUnit = null;
+        double currBestValue = 0;
+        int system = mUnit.getSystem();
+        ArrayList<Unit> unitList = unitsFromSystem(system);
+        double destValue;
+        for (Iterator<Unit> i = unitList.iterator(); i.hasNext();) {
+            Unit currUnit = i.next();
+            Log.d(LOG_NAME, "Testing unit " + currUnit.getAbbrev());
+            if (currBestUnit == null) {
+                currBestUnit = currUnit;
+                currBestValue = convert(srcValue, mMetreUnit, currUnit);
+                continue;
+            }
+            Log.d(LOG_NAME, "Best unit so far: " + currBestUnit.getAbbrev());
+            destValue = convert(srcValue, mMetreUnit, currUnit);
+            if ((destValue < LARGESTNUM) && (destValue > currBestValue || currBestValue > LARGESTNUM)) {
+                    currBestUnit = currUnit;
+                    currBestValue = destValue;
+            }
+        }
+        if (currBestUnit == null) {
+            Log.wtf(LOG_NAME, "No best unit found");
+            Log.d(LOG_NAME, "unit = " + mUnit.getAbbrev() + ", value = " + srcValue);
+        }
+        Log.d(LOG_NAME, "Best unit: " + currBestUnit.getAbbrev());
+        return currBestUnit;
+    }
+    
     public class Unit {
         String mName;
         String mAbbrev;
         double mValue;
-        String mBigger;
-        String mSmaller;
+        int mSystem;
         
-        public Unit(String name, String abbrev, double value, String bigger, String smaller) {
+        public Unit(String name, String abbrev, double value, int system) {
             mName = name;
             mAbbrev = abbrev;
             mValue = value;
-            mBigger = bigger;
-            mSmaller = smaller;
+            mSystem = system;
         }
         
         public String getName() {
@@ -135,6 +177,10 @@ public class UnitConverter
         
         public double getValue() {
             return mValue;
+        }
+        
+        public int getSystem() {
+            return mSystem;
         }
     }
 }
