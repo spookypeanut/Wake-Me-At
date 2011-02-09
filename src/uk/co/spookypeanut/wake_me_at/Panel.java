01145fa981d87d879409026c5f55940f81779c78
spookypeanut <github@spookypeanut.co.uk>
2011-02-09 22:36:38 +0000

    Unused, but valid, Panel.java

---
+++ src/uk/co/spookypeanut/wake_me_at/Panel.java
@@ -0,0 +1,58 @@
+package uk.co.spookypeanut.wake_me_at;
+/*
+    This file is part of Wake Me At. Wake Me At is the legal property
+    of its developer, Henry Bush (spookypeanut).
+
+    Wake Me At is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wake Me At is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wake Me At, in the file "COPYING".  If not, see 
+    <http://www.gnu.org/licenses/>.
+ */
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+class Panel extends SurfaceView implements SurfaceHolder.Callback {
+    public Panel(Context context) {
+        super(context);
+    }
+
+    /* (non-Javadoc)
+     * @see android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder, int, int, int)
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder)
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see android.view.SurfaceHolder.Callback#surfaceDestroyed(android.view.SurfaceHolder)
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
