476865e31eda6e9159590a9facab02981f45b236
spookypeanut <github@spookypeanut.co.uk>
2013-02-10 21:44:02 +0000

    A little cleanup

--- src/uk/co/spookypeanut/wake_me_at/Alarm.java
+++ src/uk/co/spookypeanut/wake_me_at/Alarm.java
@@ -192,9 +192,10 @@ public class Alarm extends Activity implements TextToSpeech.OnInitListener, OnUt
         
         //REF#0025
         final Window win = getWindow();
+        // This method of waking up the device seems to be required on > 4.0
         win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-        win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
  
